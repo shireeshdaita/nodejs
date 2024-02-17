698be1069fbdfced218c1901a472eadbb702a38f
shireesh <shireeshkumard@gmail.com>
2024-02-17 09:04:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var dt = require('./myowndate')
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(req.url);
   res.write('today date::'+ dt.myDateTime())
   res.end();
 }).listen(8080);
